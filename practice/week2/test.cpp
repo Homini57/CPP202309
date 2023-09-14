4d133e7cb1ec1f5780738f98a7e53d2393d8cba9
unknown <200244@jnu.ac.kr>
2023-09-14 11:37:33 +0900

    practic upload

--- practice/week2/test.cpp
+++ practice/week2/test.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main()
 {
+	// 정수 변수 정의 및 초기화
 	int a = 10;
 	int b = 15;
+
+	// cout : c++ 출력 함수
+	// endl;를 적음으로써 cout 종료
 	cout << a + b << endl;
 
 	char text1[10] = "Hello";
