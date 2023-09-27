c304be36525b64dcc3ca69fc596e35923234b790
unknown <200244@jnu.ac.kr>
2023-09-27 13:47:15 +0900

    Week4.2. 배열

--- challenge/week4/selectCustomer.cpp
+++ challenge/week4/selectCustomer.cpp
@@ -6,15 +6,16 @@ using namespace std;
 int main()
 {
 	const int maxPeople = 3;
-	int i, j = 0;
+	int i, j = 0;				// i : 반복문의 반복 횟수에 관한 변수
+								//j : 특정 나이 이상인 사람 없으면 0, 있으면 1 이상, 0이면 문장 출력
 	cout << "총 고객의 수를 입력하라: \n";
 	// cin >> maxPeople;
-	string names[maxPeople];
-	int ages[maxPeople];
-	for (i = 0; i < maxPeople; i++)
+	string names[maxPeople];	//고객 이름에 관한 배열
+	int ages[maxPeople];		//고객 나이에 관한 배열
+	for (i = 0; i < maxPeople; i++)						//다음 실행문을 maxPeople번 반복
 	{
 		cout << "사람 " << i+1 << "의 " << "이름: ";
-		cin >> names[i];
+		cin >> names[i];								//배열의 index는 0부터 시작, 1번째의 index = 0
 		cout << "사람 " << i+1 << "의 " << "나이: ";
 		cin >> ages[i];
 	}
@@ -25,19 +26,20 @@ int main()
 	cin >> ageThreshold;
 
 	cout << ageThreshold << "세 이상인 고객들:\n";
-	for (i = 0; i < maxPeople; i++)
+	for (i = 0; i < maxPeople; i++)						//모든 고객에 대하여 다음 실행문(나이 검사) 반복
 	{
-		if (ages[i] >= ageThreshold)
+		if (ages[i] >= ageThreshold)					//고객의 나이가 ageThreshold세 이상이면 문장 출력
 		{
 			cout << names[i] << " (" << ages[i] << "세)" << endl;
-			j++;
+			j++;										//ageThreshold세 이상인 고객이 있으면 j가 1 이상
+														//ageThreshold세 이상인 고객이 없으면 j = 0
 		}
 		
 		
 	}
-	if(j == 0)
+	if(j == 0)											//ageThreshold세 이상인 고객이 없으면(j = 0이면)
 	{
-		cout << ageThreshold << "이상의 나이를 가진 고객이 없습니다.";
+		cout << ageThreshold << "이상의 나이를 가진 고객이 없습니다.";		//고객이 없다는 문장 출력
 	}
 
 
--- practice/week4/foundationYear.cpp
+++ practice/week4/foundationYear.cpp
@@ -6,12 +6,12 @@ int main()
 {
 	const int kArraySize = 3;
 
-	int founding[kArraySize];
+	int founding[kArraySize];							//배열 선언 : type 배열이름[배열크기]
 	founding[0] = 918;
 	founding[1] = 1302;
 	founding[2] = 1948;
 
-	cout << "고려 건국 연도 : " << founding[0] << endl;
+	cout << "고려 건국 연도 : " << founding[0] << endl;	//배열[index]는 배열의 값을 반환한다.
 	cout << "조선 건국 연도 : " << founding[1] << endl;
 	cout << "한국 건국 연도 : " << founding[2] << endl;
 
